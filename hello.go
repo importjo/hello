5a50fdd176f80197c82eb9f109b1a53ca886a664
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2019-07-27 00:48:39 -0400

    add concurency

--- hello.go
+++ hello.go
@@ -1,10 +1,37 @@
 package main
 
-import(
-  "fmt"
-  "github.com/importjo/stringutil"
+import (
+	"fmt"
+	"time"
 )
 
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
 func main() {
-	fmt.Println(stringutil.Reverse("!oG,olleH"))
+	var c chan string = make(chan string)
+
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
 }
