b6b1735af0cfee6e86a3e66941f449f6929b3593
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2019-07-26 23:31:34 -0400

    change tuto

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import(
+  "fmt"
+  "github.com/importjo/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG,olleH"))
 }
